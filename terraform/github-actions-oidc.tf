07fd02fbd6fe3792c1856695cb81c26d5bd2f20f
Isaac Voon <isaacvoon20@fmail.com>
2026-03-26 11:09:06 +1030

    Trigger pipeline

--- terraform/github-actions-oidc.tf
+++ terraform/github-actions-oidc.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "github_actions_role" {
       {
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::040235833373:oidc-provider/token.actions.githubusercontent.com"
+          Federated = "arn:aws:iam::040235853373:oidc-provider/token.actions.githubusercontent.com"
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
